8eb6644f7a06cee231324592c398b4d87bc252bc
Päivi <paivi.nummi@helsinki.fi>
2016-10-20 17:54:38 +0300

    sivutoiminnon viilausta

--- src/main/java/tikape/runko/Main.java
+++ src/main/java/tikape/runko/Main.java
@@ -63,17 +63,18 @@ public class Main {
         get("/keskustelualue/:tunnus/:id/:sivu", (req, res) -> {
             HashMap map = new HashMap<>();
             map.put("viestit", viestiDao.findPage(Integer.parseInt(req.params("sivu")), Integer.parseInt(req.params("id"))));
+            map.put("seuraavatViestit", viestiDao.findPage((Integer.parseInt(req.params("sivu")) + 1), Integer.parseInt(req.params("id"))));
             map.put("id", req.params("id"));
             map.put("tunnus", req.params("tunnus"));
-            map.put("sivu", (Integer.parseInt(req.params("sivu")) + 1));
+            map.put("sivu", (Integer.parseInt(req.params("sivu"))));
             return new ModelAndView(map, "viestiketju");
         }, new ThymeleafTemplateEngine());
         
-        post("/keskustelualue/:tunnus/:id", (req, res) -> {
+        post("/keskustelualue/:tunnus/:id/:sivu", (req, res) -> {
             String viesti = req.queryParams("viesti");
             String lahettaja = req.queryParams("lahettaja");
             viestiDao.submitViesti(Integer.parseInt(req.params("id")), viesti, lahettaja);
-            res.redirect("/keskustelualue/" + req.params("tunnus") + "/" + req.params("id"));
+            res.redirect("/keskustelualue/" + req.params("tunnus") + "/" + req.params("id") + "/"  + req.params("sivu"));
             return "ok";
         });
     }
